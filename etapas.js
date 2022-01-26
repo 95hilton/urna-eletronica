68c316a28ce745451f847a406c74058ce1fa2b9d
Hilton Gonçalves <hiltong8@gmail.com>
2022-01-26 16:57:20 -0300

    Initial commit

---
+++ etapas.js
@@ -0,0 +1,52 @@
+let etapas =[ 
+         {
+        titulo: "VEREADOR",
+        numeros: 5,
+        candidatos: [ 
+             {
+                numero: '38111',
+                nome: "Christianne Varão",
+                partido: "PEN",
+                foto: [
+                    {url:"candidato-1.jpg", legenda: "Vereador"}
+                ]
+            },
+            {
+                numero: '44444',
+                nome: "Maria José",
+                partido: "PQP",
+                foto: [
+                    {url:"candidato-2.jpg", legenda: "Vereador"}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: "PREFEITO",
+        numeros: 2,
+        candidatos: [ 
+             {
+                numero: '10',
+                nome: "Fernadooo",
+                partido: "PEN",
+                vice: "Cic",
+                fotos: [
+                    {url:"candidato-1.jpg", legenda: "Prefeito"},
+                    {url:"candidato-2.jpg", legenda: "Vice-Prefeito", small: true}
+                ]
+            },
+            {
+                numero: '11',
+                nome: "Perfeito",
+                partido: "PQP",
+                vice: "Cic",
+                fotos: [
+                    {url:"candidato-1.jpg", legenda: "Prefeito"},
+                    {url:"candidato-2.jpg", legenda: "Vice-Prefeito", small: true}
+                ]
+            },
+        
+
+                ]
+    },
+]
